ee198790ef1d2faceef4cf4e78de1a7c1479e6d9
Pascal Hürten <pascal.huerten@th-luebeck.de>
2024-07-13 13:17:17 +0200

    Add basic hud

--- source/main.lua
+++ source/main.lua
@@ -4,10 +4,11 @@ import "CoreLibs/object"
 import "object"
 import "ship"
 import "input"
+import "hud" -- DEMO
+local hud = hud() -- DEMO
 
 local gfx <const> = playdate.graphics
 local dt <const> = playdate.getElapsedTime()
-local font = gfx.font.new('font/Mini Sans 2X') -- DEMO
 local shifParams = {
 	x = 200;
     y = 200;
@@ -20,7 +21,9 @@ local shifParams = {
 local ship = ship(shifParams)
 
 local function loadGame()
-	
+	playdate.display.setRefreshRate(50) -- Sets framerate to 50 fps
+	math.randomseed(playdate.getSecondsSinceEpoch()) -- seed for math.random
+	-- gfx.setFont(font) -- DEMO
 end
 
 loadGame()
@@ -28,12 +31,19 @@ loadGame()
 local function updateGame()
 	params = {deltaTime = dt;}
 	ship:update(params)
+	local cannonCount = 1
+	local sailCount = 5
+	hud:update({
+		cannonCount = cannonCount,
+		sailCount = sailCount
+	})
 end
 
 local function drawGame()
 	gfx.clear() -- Clears the screen
 	ship:draw(0,0)
 	gfx.sprite.update()
+	hud:draw()
 end
 
 function playdate.update()
